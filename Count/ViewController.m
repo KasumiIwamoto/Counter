7250a23e56d630803d6483a89483251047b83e1b
岩本果純 <iwamotokasumi@iwamotokajun-no-MacBook-Air.local>
2015-03-19 14:13:37 +0900

    Initial Commit

---
+++ Count/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Count
+//
+//  Created by 岩本果純 on 2015/03/19.
+//  Copyright (c) 2015年 KasumiIwamoto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Count/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Count
+//
+//  Created by 岩本果純 on 2015/03/19.
+//  Copyright (c) 2015年 KasumiIwamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
